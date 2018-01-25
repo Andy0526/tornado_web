3cd5f9c9dbdea77c89d7ae316fe29c5505692ec7
lewsan <liujintong@peiwo.cn>
2018-01-25 21:49:47 +0800

    ADD:add read_write in database

--- tutorial/databases/definitions_readwrite.py
+++ tutorial/databases/definitions_readwrite.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # author:lewsan
-import sys
 import pymongo
 import tornado.web
 import tornado.ioloop
@@ -9,9 +8,8 @@ import tornado.options
 import tornado.httpserver
 
 from tornado.options import define, options
-from tutorial.databases.config import MONGO_SERVER
 
-sys.path.append('..')
+from tutorial.databases.config import MONGO_SERVER
 
 
 def make_parser():
@@ -27,33 +25,37 @@ class WordHandler(tornado.web.RequestHandler):
             self.write(word_doc)
         else:
             self.set_status(404)
-            self.write({"error": "word not found"})
 
-    @classmethod
-    def findOne(cls, coll, spec):
-        word_doc = coll.find_one(spec)
+    def post(self, word):
+        definition = self.get_argument('definition')
+        coll = self.application.db.words
+        word_doc = coll.find_one({'word': word})
         if word_doc:
-            del word_doc['id']
-        return word_doc
+            word_doc['definition'] = definition
+            coll.save(word_doc)
+        else:
+            word_doc = {
+                'word': word,
+                'definition': definition
+            }
+            coll.insert(word_doc)
+        del word_doc['_id']
+        self.write(word_doc)
 
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r'/(\w+)', WordHandler),
+            (r"/(\w+)", WordHandler),
         ]
         conn = pymongo.MongoClient(MONGO_SERVER['host'], MONGO_SERVER['port'])
         self.db = conn[MONGO_SERVER['database']]
         tornado.web.Application.__init__(self, handlers)
 
 
-def main():
+if __name__ == '__main__':
     make_parser()
-    tornado.options.parse_command_line()
+    options.parse_command_line()
     server = tornado.httpserver.HTTPServer(Application())
     server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
-
-
-if __name__ == '__main__':
-    main()
